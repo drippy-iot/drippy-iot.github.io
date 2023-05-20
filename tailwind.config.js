8eff37078978080d701a1a4324a026a68ba91fd3
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-20 20:05:15 +0800

    feat: add tailwind wrapper component via plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+import plugin from 'tailwindcss/plugin';
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: ['./src/**/*.{html,js,ts,svelte}'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        plugin(({ addComponents }) =>
+            addComponents({
+                '.wrapper': {
+                    height: '100%',
+                    width: '100%',
+                },
+            })
+        ),
+    ],
 };
