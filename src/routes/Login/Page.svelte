724aa45a32109f55edc8c18013c1b7f46c626a7e
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-21 02:31:36 +0800

    feat: use animated blob for login background

--- src/routes/Login/Page.svelte
+++ src/routes/Login/Page.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
     import FaUser from 'svelte-icons/fa/FaUser.svelte';
     import FaShieldAlt from 'svelte-icons/fa/FaShieldAlt.svelte';
-    import { link } from 'svelte-spa-router';
 
     import Layout from '../../components/Layout.svelte';
     import TabItem from '../../components/Tab/TabItem.svelte';
     import TabGroup from '../../components/Tab/TabGroup.svelte';
+    import { TOP_BLOB, BOT_BLOB } from './blobPaths';
+    import AnimatedBlob from '../../components/AnimatedBlob.svelte';
 
     let isUser = true;
 </script>
 
 <Layout>
     <h1>Login Page: {isUser}</h1>
+    <div class="blob top">
+        <AnimatedBlob ds={TOP_BLOB} duration="5s" />
+    </div>
+    <div class="blob bot">
+        <AnimatedBlob ds={BOT_BLOB} duration="7s" />
+    </div>
     <TabGroup>
         <TabItem on:click={() => (isUser = true)} active={isUser}>
             <FaUser />
@@ -21,3 +28,18 @@
         </TabItem>
     </TabGroup>
 </Layout>
+
+<style>
+    .blob {
+        @apply text-blue-300;
+        @apply fixed z-[-999];
+        @apply w-[150%];
+    }
+    .top {
+        @apply left-[-60%] top-[-34%];
+    }
+    .bot {
+        @apply visible;
+        @apply bottom-[-20%] right-[-60%];
+    }
+</style>
