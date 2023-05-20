25d621163e33d8742a5c8c3e6166aa26013db496
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-21 02:31:16 +0800

    feat: add animated blob based on mmfallacy/svelte-mobilewrapper-blob

---
+++ src/components/AnimatedBlob.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let ds: string[];
+    export let duration: string;
+</script>
+
+<svg
+    viewBox="0 0 200 200"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path fill="currentColor" transform="translate(100 100)">
+        <animate
+            attributeName="d"
+            dur={duration}
+            repeatCount="indefinite"
+            values={[...ds, ds[0]].join(';')}
+        />
+    </path>
+</svg>
