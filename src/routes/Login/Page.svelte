d7e0796b3f815f5fc8401773fcbd87be5a0b643b
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-21 01:38:57 +0800

    feat: make login page use tab component

--- src/routes/Login/Page.svelte
+++ src/routes/Login/Page.svelte
@@ -1,9 +1,23 @@
-<script>
-    import Layout from '../../components/Layout.svelte';
+<script lang="ts">
+    import FaUser from 'svelte-icons/fa/FaUser.svelte';
+    import FaShieldAlt from 'svelte-icons/fa/FaShieldAlt.svelte';
     import { link } from 'svelte-spa-router';
+
+    import Layout from '../../components/Layout.svelte';
+    import TabItem from '../../components/Tab/TabItem.svelte';
+    import TabGroup from '../../components/Tab/TabGroup.svelte';
+
+    let isUser = true;
 </script>
 
 <Layout>
-    <h1>Login Page</h1>
-    <a use:link href="/dash">Dashboard</a>
+    <h1>Login Page: {isUser}</h1>
+    <TabGroup>
+        <TabItem on:click={() => (isUser = true)} active={isUser}>
+            <FaUser />
+        </TabItem>
+        <TabItem on:click={() => (isUser = false)} active={!isUser}>
+            <FaShieldAlt />
+        </TabItem>
+    </TabGroup>
 </Layout>
