06b86d82200169b0a5d9679c58e9aa7b4c15ddd1
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-21 13:46:54 +0800

    fmt: apply formatting fixes

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 
     import Dashboard from './routes/Dashboard/Page.svelte';
     import Login from './routes/Login/Page.svelte';
+
     import { register } from './register.ts';
 
     const routes = {
@@ -14,7 +15,7 @@
 <main>
     {#await register()}
         Loading service worker.
-    {:then} 
+    {:then}
         Loaded.
         <Router {routes} />
     {/await}
--- src/sw.ts
+++ src/sw.ts
@@ -1,35 +1,31 @@
-import { manifest, version } from "@parcel/service-worker";
-import { assert } from "./assert";
+import { assert } from './assert.ts';
+import { manifest, version } from '@parcel/service-worker';
 
 async function installWorker() {
-    const INDEX  = '/index.html';
-    const files = manifest.map(path => {
-        if (path.endsWith(INDEX))
-            return path.slice(0, -INDEX.length) || '/';
-        return path;
-    });
-
     // Get all the new assets
     const cache = await caches.open(version);
-    return cache.addAll(files);
+    return cache.addAll(manifest);
 }
 
 async function activateWorker() {
     const keys = await caches.keys();
-    const deletePromises = keys.map(key => {
+    const deletePromises = keys.map((key) => {
         if (key !== version) return caches.delete(key);
-    })
-    const results = await Promise.all(deletePromises)
-    assert(results.every(x => x));
+    });
+    const results = await Promise.all(deletePromises);
+    assert(results.every((x) => x));
 }
 
-self.addEventListener('install', evt => {
-    assert(evt instanceof ExtendableEvent);
-    evt.waitUntil(installWorker());
-}, { once: true, passive: true });
+self.addEventListener(
+    'install',
+    (evt) => {
+        assert(evt instanceof ExtendableEvent);
+        evt.waitUntil(installWorker());
+    },
+    { once: true, passive: true }
+);
 
-self.addEventListener('activate', evt => {
+self.addEventListener('activate', (evt) => {
     assert(evt instanceof ExtendableEvent);
     evt.waitUntil(activateWorker());
-})
-
+});
