cc8d09aab771a2dc8dc0bd0fe263b0065099fe12
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-22 18:37:48 +0800

    feat: add option picker

---
+++ src/components/Select/Options.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let open: boolean;
+    let dialog: HTMLDialogElement;
+
+    $: if (dialog && open) dialog.showModal();
+</script>
+
+<dialog
+    bind:this={dialog}
+    on:click|self={() => dialog.close()}
+    on:close={() => (open = false)}
+>
+    <div class="wrapper" on:click|stopPropagation>wqojoejqw</div>
+</dialog>
