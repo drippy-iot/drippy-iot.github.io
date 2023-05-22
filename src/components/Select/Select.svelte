bd9772c3de3cc312f6b5e18a8d6b876da7bef8b5
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-22 15:47:52 +0800

    feat: select component styling and value prop

--- src/components/Select/Select.svelte
+++ src/components/Select/Select.svelte
@@ -1,12 +1,24 @@
 <script>
     export let name;
+    export let value;
 </script>
 
 <button>
     <label for={name}> Device Mac:</label>
 
-    <input type="text" {name} />
+    <input type="text" {name} bind:value />
 </button>
 
 <style>
+    button {
+        @apply px-6 py-2;
+        @apply bg-slate-700;
+        @apply rounded-full;
+        @apply text-white;
+    }
+
+    input {
+        @apply bg-transparent;
+        @apply text-center font-thin;
+    }
 </style>
