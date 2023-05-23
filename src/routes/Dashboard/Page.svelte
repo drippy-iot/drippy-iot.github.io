78fc476c9a916a66d2aac86edcb2bd7400b03270
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-23 16:44:49 +0800

    feat: use waves to create Dashboard's background component

--- src/routes/Dashboard/Page.svelte
+++ src/routes/Dashboard/Page.svelte
@@ -1,7 +1,12 @@
 <script>
     import Layout from '../../components/Layout.svelte';
+    import Background from './Background.svelte';
 </script>
 
 <Layout>
     <h1>Dashboard Page</h1>
+    <Background />
 </Layout>
+
+<style>
+</style>
