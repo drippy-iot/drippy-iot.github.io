788c905a9c7df1bd5be11626314958240e745bdd
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-23 16:42:45 +0800

    feat: waves component based on Blob.svelte

---
+++ src/components/Waves.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let ds: string[];
+    export let duration: string;
+</script>
+
+<svg
+    viewBox="0 0 1440 320"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path fill="currentColor">
+        <animate
+            attributeName="d"
+            dur={duration}
+            repeatCount="indefinite"
+            values={[...ds, ds[0]].join(';')}
+        />
+    </path>
+</svg>
