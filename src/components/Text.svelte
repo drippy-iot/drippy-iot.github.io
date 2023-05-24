58afe1e9e1cb12333b67e1e717726c4b6a56ae71
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-25 01:53:47 +0800

    feat: pass foreground color as css variable text component

--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -5,6 +5,7 @@
 <style>
     span {
         background: var(--text-bg, white);
+        color: var(--text-fg, theme(colors.slate.900));
         @apply flex items-center rounded-full px-4 py-1;
     }
 </style>
