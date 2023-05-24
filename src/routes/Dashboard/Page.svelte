62dc50e784ca42dfad36bf19156c0d2946832bee
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-24 23:40:10 +0800

    style: forego markup and style decoupling for dashboard page

--- src/routes/Dashboard/Page.svelte
+++ src/routes/Dashboard/Page.svelte
@@ -3,30 +3,20 @@
     import Select from '../../components/Select/Select.svelte';
     import Text from '../../components/Tab/Text.svelte';
     import Background from './Background.svelte';
+    import COLORS from 'tailwindcss/colors';
 
     const options = ['ad:ad:ad:ad', '00:00:00:00', 'co:de:ba:be'];
     let value = options[0];
 </script>
 
 <Layout>
-    <div class="wrapper">
+    <div class="wrapper flex flex-col items-center p-4 text-sm">
         <h1>Dashboard Page</h1>
 
-        <span class="select-row">
+        <span class="flex w-full justify-between">
             <Select {options} bind:value disabled>Device Mac:</Select>
-            <Text>Connected</Text>
+            <Text --text-bg={COLORS.green[500]}>Connected</Text>
         </span>
         <Background />
     </div>
 </Layout>
-
-<style>
-    div.wrapper {
-        @apply flex flex-col items-center p-4;
-        @apply text-sm;
-    }
-    .select-row {
-        @apply flex w-full justify-between;
-        --text-bg: theme(colors.green.500);
-    }
-</style>
