b74efc008e554a50bc59915d2b1f04d004063cfd
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-25 00:30:49 +0800

    feat: add tabgroup and adjust first spacer

--- src/routes/Dashboard/Page.svelte
+++ src/routes/Dashboard/Page.svelte
@@ -1,6 +1,9 @@
 <script>
     import Layout from '../../components/Layout.svelte';
     import Select from '../../components/Select/Select.svelte';
+    import Spacer from '../../components/Spacer.svelte';
+    import TabGroup from '../../components/Tab/TabGroup.svelte';
+    import TabItem from '../../components/Tab/TabItem.svelte';
     import Text from '../../components/Text.svelte';
     import Background from './Background.svelte';
     import COLORS from 'tailwindcss/colors';
@@ -15,11 +18,17 @@
             <h2>Welcome</h2>
             <h1>Some-Dood</h1>
         </span>
+        <Spacer --length="25cqh" />
         <span class="flex w-full justify-between">
             <Select {options} bind:value disabled>Device Mac:</Select>
             <Text --text-bg={COLORS.green[500]}>Connected</Text>
         </span>
+        <Spacer />
         <h2>System Log:</h2>
+        <TabGroup>
+            <TabItem>Dashboard</TabItem>
+            <TabItem>Settings</TabItem>
+        </TabGroup>
         <Background />
     </div>
 </Layout>
