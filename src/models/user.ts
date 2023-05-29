3c2890c95581f7652e68d7c1d9dee7a02c84f728
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-30 03:09:06 +0800

    feat(model): implement Zod schemas for SSE messages

---
+++ src/models/user.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+export const FlowSchema = z.object({
+    ty: z.literal('flow'),
+    ts: z.coerce.date(),
+    flow: z.number().int().nonnegative(),
+});
+export type Flow = z.infer<typeof FlowSchema>;
+
+export const LeakSchema = z.object({
+    ty: z.literal('leak'),
+    ts: z.coerce.date(),
+});
+export type Leak = z.infer<typeof LeakSchema>;
+
+export const ControlSchema = z.object({
+    ty: z.literal('control'),
+    ts: z.coerce.date(),
+    shutdown: z.boolean(),
+});
+export type Control = z.infer<typeof ControlSchema>;
+
+export const UserMessageSchema = z.union([
+    FlowSchema,
+    LeakSchema,
+    ControlSchema,
+]);
+export type UserMessage = z.infer<typeof UserMessageSchema>;
