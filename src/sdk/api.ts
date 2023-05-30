27e930fcfb950bacc7846a68ab4e1a5cc8881805
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-30 14:48:37 +0800

    feat(sdk): implement wrapper for reset request

--- src/sdk/api.ts
+++ src/sdk/api.ts
@@ -4,6 +4,31 @@ import { InvalidSession, UnexpectedStatusCode } from './error.ts';
 
 /**
  * Assuming that the user has already logged in (i.e., there exists a valid session ID
+ * in the cookie store), this endpoint sends a request to the Cloud to reset or manually
+ * bypass a locked unit (i.e., "closed valve").
+ *
+ * Returns `true` if the unit was previously locked. Returns `false` if the device is
+ * already unlocked (due to a previous invocation for example).
+ */
+export async function requestReset(): Promise<boolean> {
+    const { status } = await fetch('/api/reset', {
+        method: 'POST',
+        credentials: 'include',
+    });
+    switch (status) {
+        case StatusCodes.OK:
+            return true;
+        case StatusCodes.ACCEPTED:
+            return false;
+        case StatusCodes.UNAUTHORIZED:
+            throw new InvalidSession();
+        default:
+            throw new UnexpectedStatusCode(status);
+    }
+}
+
+/**
+ * Assuming that the user has already logged in (i.e., there exists a valid session ID
  * in the cookie store), this endpoint sends a request to the Cloud to shut down the
  * device associated with the session.
  *
