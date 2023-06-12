98511e235031e8d71b12b142225a05861d0df729
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-06-12 21:08:54 +0800

    fix(sw): allow page to be loaded offline

--- src/sw.ts
+++ src/sw.ts
@@ -3,30 +3,45 @@ import { manifest, version } from '@parcel/service-worker';
 import { assert } from './assert.ts';
 
 async function installWorker() {
-    // Get all the new assets
+    const files = manifest.map(path => {
+        const INDEX = '/index.html';
+        return path.endsWith(INDEX)
+            ? path.slice(0, -INDEX.length) || '/'
+            : path;
+    });
     const cache = await caches.open(version);
-    return cache.addAll(manifest);
+    return cache.addAll(files);
+}
+
+function* deleteAll(keys: Iterable<string>) {
+    for (const key of keys)
+        if (key !== version)
+            yield caches.delete(key);
 }
 
 async function activateWorker() {
     const keys = await caches.keys();
-    const deletePromises = keys.map(key =>
-        key === version ? Promise.resolve(true) : caches.delete(key)
-    );
-    const results = await Promise.all(deletePromises);
+    const results = await Promise.all(deleteAll(keys));
     assert(results.every(x => x));
 }
 
-self.addEventListener(
-    'install',
-    evt => {
-        assert(evt instanceof ExtendableEvent);
-        evt.waitUntil(installWorker());
-    },
-    { once: true, passive: true }
-);
+async function interceptFetch(req: Request) {
+    const cache = await caches.open(version);
+    const res = await cache.match(req);
+    return res ?? fetch(req);
+}
+
+self.addEventListener('install', evt => {
+    assert(evt instanceof ExtendableEvent);
+    evt.waitUntil(installWorker());
+}, { once: true, passive: true });
 
 self.addEventListener('activate', evt => {
     assert(evt instanceof ExtendableEvent);
     evt.waitUntil(activateWorker());
-});
+}, { once: true, passive: true });
+
+self.addEventListener('fetch', evt => {
+    assert(evt instanceof FetchEvent);
+    evt.respondWith(interceptFetch(evt.request));
+}, { passive: true });
