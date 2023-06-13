0436558c086139f7bfa1523ed5fcc3bbe15cc4f4
Marc <mgc.macaraeg@gmail.com>
2023-06-14 03:55:14 +0800

    feat: put function declarations inline

--- src/stores/usermetrics.ts
+++ src/stores/usermetrics.ts
@@ -3,69 +3,61 @@ import { Bypass, Close, Flow, Open } from '../models/user';
 import { MetricsListener } from '../sdk/metrics';
 
 interface MetricEvent {
-    ty: "close" | "bypass" | "open" | "flow"
-    ts: Date
-    flow?: number
+    ty: 'close' | 'bypass' | 'open' | 'flow';
+    ts: Date;
+    flow?: number;
 }
 
-export const userMetricsFlow = writable<Flow[]>()
-export const userMetricsEvents = writable<MetricEvent[]>()
-
-function onFlows (flow: Flow[]) {
-    userMetricsFlow.update(arr => {
-        arr.push(...flow)
-        return arr
-    })
-    // Assume that any leak event that ends up in userMetricsEvent is a leak.
-    const leaks = flow.filter(record => record.flow)
-    if (leaks) {
-        const eventFmt = leaks.map(leak => {
-            return {
-                ty: "flow",
-                ts: leak.end,
-                flow: leak.flow
-            } as MetricEvent
-        })
-        userMetricsEvents.update(arr => {
-            arr.push(...eventFmt)
-            return arr;
-        })
-    }
-}
-
-function onOpen(ts: Open) {
-    userMetricsEvents.update(arr => {
-        arr.push({
-            ty: "open",
-            ts,
-        })
-        return arr;
-    })
-}
-
-function onBypass(ts: Bypass) {
-    userMetricsEvents.update(arr => {
-        arr.push({
-            ty: "bypass",
-            ts,
-        })
-        return arr;
-    })
-}
-
-function onClose(ts: Close) {
-    userMetricsEvents.update(arr => {
-        arr.push({
-            ty: "close",
-            ts,
-        })
-        return arr;
-    })
-}
+export const userMetricsFlow = writable<Flow[]>();
+export const userMetricsEvents = writable<MetricEvent[]>();
 
 export const userMetricsListener: MetricsListener = {
-    onFlows,
-    onOpen,
-    onBypass,
-    onClose,
+    onFlows(flows) {
+        userMetricsFlow.update(arr => {
+            arr.push(...flows);
+            return arr;
+        });
+        // Assume that any leak event that ends up in userMetricsEvent is a leak.
+        const leaks = flows.filter(record => record.flow);
+        if (leaks) {
+            const eventFmt = leaks.map(leak => {
+                return {
+                    ty: 'flow',
+                    ts: leak.end,
+                    flow: leak.flow,
+                } as MetricEvent;
+            });
+            userMetricsEvents.update(arr => {
+                arr.push(...eventFmt);
+                return arr;
+            });
+        }
+    },
+    onOpen(ts) {
+        userMetricsEvents.update(arr => {
+            arr.push({
+                ty: 'open',
+                ts,
+            });
+            return arr;
+        });
+    },
+    onBypass(ts) {
+        userMetricsEvents.update(arr => {
+            arr.push({
+                ty: 'bypass',
+                ts,
+            });
+            return arr;
+        });
+    },
+    onClose(ts) {
+        userMetricsEvents.update(arr => {
+            arr.push({
+                ty: 'close',
+                ts,
+            });
+            return arr;
+        });
+    },
 };
