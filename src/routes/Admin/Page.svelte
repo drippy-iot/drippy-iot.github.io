0a35323dabe5060d31ef65854f201450dcefced9
Marc Macaraeg <mcmacaraeg2@up.edu.ph>
2023-06-14 13:32:27 +0800

    feat: remove more sesison logic in admin

--- src/routes/Admin/Page.svelte
+++ src/routes/Admin/Page.svelte
@@ -21,13 +21,6 @@
     import Display from '../../components/Chart/Display.svelte';
     import { GRAN_OPTS } from './constants';
     import { Granularity } from '../../components/Chart/types';
-    import { logout } from '../../sdk/auth';
-
-    // Redirect to Login on no session
-
-    const sessionReady = session.reload?.().then(session => {
-        if (session === null) replace('/');
-    });
 
     // Flatten objects to array of labels.
     let GRANULARITY_OPTIONS = GRAN_OPTS.map(option => option.label);
@@ -57,55 +50,47 @@
     ).catch(console.error);
 </script>
 
-{#await sessionReady}
-    <p>Loading session.</p>
-{:then}
-    <Layout>
-        <div class="wrapper flex flex-col gap-4 p-4 text-xs">
+<Layout>
+    <div class="wrapper flex flex-col gap-4 p-4 text-xs">
+        <div>
+            <h2>Welcome</h2>
+            <h1>Administrator</h1>
+        </div>
+        <div class="relative -left-4 max-h-[30cqh] w-[100cqw]">
+            <Display flowDataSource={$systemMetricsFlow} />
+            <span class="absolute bottom-full right-4">
+                <Select
+                    name="granularity"
+                    bind:value
+                    options={GRANULARITY_OPTIONS}
+                />
+            </span>
+        </div>
+        <div class="flex justify-between">
             <div>
-                <h2>Welcome</h2>
-                <h1>Administrator</h1>
+                System Wide View
             </div>
-            <div class="relative -left-4 max-h-[30cqh] w-[100cqw]">
-                <Display flowDataSource={$systemMetricsFlow} />
-                <span class="absolute bottom-full right-4">
-                    <Select
-                        name="granularity"
-                        bind:value
-                        options={GRANULARITY_OPTIONS}
-                    />
-                </span>
-            </div>
-            {#if $session !== null}
-                <div class="flex justify-between">
-                    <div>
-                        Device Mac: {$session.mac}
-                    </div>
-                    <Text --text-bg={COLORS.green[500]}>Connected</Text>
-                </div>
-            {/if}
-            <h2 class="block">System Log:</h2>
-            <div class="flex flex-1 flex-col gap-2 overflow-y-auto">
-                {#each $systemMetricsEvents as metricEvent}
-                    <LogItem {...metricEvent} />
-                {:else}
-                    <p>No events logged yet.</p>
-                {/each}
-            </div>
+            <Text --text-bg={COLORS.green[500]}>Connected</Text>
         </div>
-        <div class="fixed bottom-0 flex h-12 w-full bg-slate-900">
-            <button
-                on:click={handleLogout}
-                class="absolute bottom-0 right-4 top-0 my-auto h-5 w-5 text-white"
-            >
-                <FaSignOutAlt />
-            </button>
+        <h2 class="block">System Log:</h2>
+        <div class="flex flex-1 flex-col gap-2 overflow-y-auto">
+            {#each $systemMetricsEvents as metricEvent}
+                <LogItem {...metricEvent} />
+            {:else}
+                <p>No events logged yet.</p>
+            {/each}
         </div>
-        <Background />
-    </Layout>
-{:catch err}
-    <p>{err}</p>
-{/await}
+    </div>
+    <div class="fixed bottom-0 flex h-12 w-full bg-slate-900">
+        <button
+            on:click={handleLogout}
+            class="absolute bottom-0 right-4 top-0 my-auto h-5 w-5 text-white"
+        >
+            <FaSignOutAlt />
+        </button>
+    </div>
+    <Background />
+</Layout>
 
 <style>
     .wrapper {
